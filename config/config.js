6e3ffd1492af5861c2cd5efd62e5e52ea23e6b69
luisgereda <luisgereda30@hotmail.com>
2021-07-02 14:10:11 +0200

    server done

--- config/config.js
+++ config/config.js
@@ -1,23 +1,24 @@
 // config.js
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "url": process.env.DB_URL,
-    "dialect": "postgres"
+  development: {
+    url: process.env.DB_URL,
+    dialect: "postgres",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
